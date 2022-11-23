46e668d75020400aece637247327281a9eb48ce1
Dmitry Poddubnov <boddubnovd@yandex.ru>
2022-11-23 23:39:00 +0300

    Решение задачи_8

---
+++ practical_task/task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int startnum = 1;
+
+
+while(startnum <= num)
+    {
+        if(startnum % 2 ==0)
+        Console.Write(startnum + ", ");
+        startnum ++;
+    }
